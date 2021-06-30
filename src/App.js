b2286fdd613b3368a431fc9a5d5aafdc9b9badd4
Nyberg5000 <82217232+Nyberg5000@users.noreply.github.com>
2021-06-30 11:46:20 +0200

    added github repository link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
       Weather app search engine
       <p>
         Coded by Tammi Nyberg{" "}
-        <a href="" target="_blank">
+        <a
+          href="https://github.com/Nyberg5000/weather-app-react-new"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on Github
         </a>
       </p>
