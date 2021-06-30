2a610be9c2e30f77f0273e6eff97cad7f3e8b0af
Nyberg5000 <82217232+Nyberg5000@users.noreply.github.com>
2021-06-30 11:40:34 +0200

    added link to github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app search engine</div>;
+  return (
+    <div className="App">
+      Weather app search engine
+      <p>
+        Coded by Tammi Nyberg{" "}
+        <a href="" target="_blank">
+          open-sourced on Github
+        </a>
+      </p>
+    </div>
+  );
 }
