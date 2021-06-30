37fb379641b99326a63cfc74d27d392002eab288
Nyberg5000 <82217232+Nyberg5000@users.noreply.github.com>
2021-06-30 12:31:19 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app search engine
-      <p>
-        Coded by Tammi Nyberg{" "}
-        <a
-          href="https://github.com/Nyberg5000/weather-app-react-new"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </p>
+      <div className="container">
+        Weather app search engine
+        <p>
+          Coded by Tammi Nyberg{" "}
+          <a
+            href="https://github.com/Nyberg5000/weather-app-react-new"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
